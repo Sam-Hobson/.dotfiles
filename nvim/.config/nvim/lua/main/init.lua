662af2cf7b5ab63fe90007b7b62f55d0bbd5e436
Sam Hobson <shhobson@protonmail.com>
2023-11-18 18:41:46 +1100

    Added new nvim setup

---
+++ nvim/.config/nvim/lua/main/init.lua
@@ -0,0 +1,39 @@
+require("main.set")
+require("main.remap")
+
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+local whitespace = augroup('MyGroup', {})
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 150,
+        })
+    end,
+})
+
+
+-- Delete trailing whitespace
+autocmd({"BufWritePre"}, {
+    group = whitespace,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
---
+++ nvim/.config/nvim/lua/main/remap.lua
@@ -0,0 +1,43 @@
+
+vim.g.mapleader = " "
+
+
+-- Open the explorer
+vim.keymap.set("n", "<leader><space>", vim.cmd.Ex)
+
+
+-- Drag lines around
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")  -- Cursor doesn't move for J
+vim.keymap.set("n", "n", "nzzzv")  -- Centre screen with n
+vim.keymap.set("n", "N", "Nzzzv")
+
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Paste over stuff in visual mode without overwriting paste register
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+
+vim.keymap.set("n", "<leader>pp", "<cmd>lua require('main.packer')<CR>");
+
