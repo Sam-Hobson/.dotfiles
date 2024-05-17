6fcb4259646c3cca183d37cc43f07e94f735d041
Sam Hobson <sam.hobson@planetinnovation.com.au>
2024-05-17 13:03:41 +1000

    Added fileformat to nvim statusbar

--- nvim/lua/main/set.lua
+++ nvim/lua/main/set.lua
@@ -16,6 +16,9 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+vim.opt.fileformats = { "unix", "dos" }
+
+vim.opt.statusline = "%<%f %h%m%r%=%-14.(%l,%c%V%) %P [%{&fileformat}]"
 
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
