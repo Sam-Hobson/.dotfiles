f250b146596420769c5ecbc8f8cefd68a229fd49
sam hobson <shhobson@proton.me>
2024-11-02 19:53:15 +1100

    Add neovim markdown preview

--- nvim/lua/main/packer.lua
+++ nvim/lua/main/packer.lua
@@ -4,62 +4,61 @@
 -- vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.4',
+		-- or                            , branch = '0.1.x',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	}
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	use { "ellisonleao/gruvbox.nvim" }
 
-  use { "ellisonleao/gruvbox.nvim" }
+	use("folke/trouble.nvim")
 
-  use("folke/trouble.nvim")
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use("theprimeagen/harpoon")
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
+	use("sindrets/diffview.nvim")
+	use("nvim-treesitter/nvim-treesitter-context");
 
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v1.x',
+		requires = {
+			-- LSP Support
+			{ 'neovim/nvim-lspconfig' },
+			{ 'williamboman/mason.nvim' },
+			{ 'williamboman/mason-lspconfig.nvim' },
 
-  use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
-  -- use("nvim-treesitter/playground")
-  use("theprimeagen/harpoon")
-  -- use("theprimeagen/refactoring.nvim")
-  use("mbbill/undotree")
-  use("tpope/vim-fugitive")
-  use("sindrets/diffview.nvim")
-  use("nvim-treesitter/nvim-treesitter-context");
-  -- use("windwp/nvim-ts-autotag")
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' },
+			{ 'hrsh7th/cmp-buffer' },
+			{ 'hrsh7th/cmp-path' },
+			{ 'saadparwaiz1/cmp_luasnip' },
+			{ 'hrsh7th/cmp-nvim-lsp' },
+			{ 'hrsh7th/cmp-nvim-lua' },
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+			-- Snippets
+			{ 'L3MON4D3/LuaSnip' },
+			{ 'rafamadriz/friendly-snippets' },
+		}
+	}
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+	use("terrortylor/nvim-comment")
+	use({ "kylechui/nvim-surround", tag = "*" })
+	use("nvim-tree/nvim-web-devicons")
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
-  }
+	use("nguyenvukhang/nvim-toggler")
 
-  use("terrortylor/nvim-comment")
-  use({ "kylechui/nvim-surround", tag = "*" })
-  use("nvim-tree/nvim-web-devicons")
+	-- Markdown
+	use("dhruvasagar/vim-table-mode")
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function() vim.fn["mkdp#util#install"]() end,
+	})
 
-  use("nguyenvukhang/nvim-toggler")
-  use("dhruvasagar/vim-table-mode")
-
-  use("sam-hobson/extended_attributes.nvim")
-
+	use("sam-hobson/extended_attributes.nvim")
 end)
-
