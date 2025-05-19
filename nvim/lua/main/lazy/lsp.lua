5c29dc0253c520f12e2fe45208303c282424820b
Sam Hobson <sam@halehobson.com>
2025-05-20 09:30:23 +1000

    Update completions

--- nvim/lua/main/lazy/lsp.lua
+++ nvim/lua/main/lazy/lsp.lua
@@ -106,5 +106,34 @@ return {
 				prefix = "",
 			},
 		})
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("user_lsp_attach", { clear = true }),
+			callback = function(event)
+				local opts = { buffer = event.buf }
+
+				vim.keymap.set("n", "K", function()
+					vim.lsp.buf.hover()
+				end, opts)
+				vim.keymap.set("n", "<leader>cc", function()
+					vim.diagnostic.open_float()
+				end, opts)
+				vim.keymap.set("n", "[d", function()
+					vim.diagnostic.goto_next()
+				end, opts)
+				vim.keymap.set("n", "]d", function()
+					vim.diagnostic.goto_prev()
+				end, opts)
+				vim.keymap.set("n", "<leader>ca", function()
+					vim.lsp.buf.code_action()
+				end, opts)
+				vim.keymap.set("n", "<leader>cr", function()
+					vim.lsp.buf.rename()
+				end, opts)
+				vim.keymap.set("i", "<C-k>", function()
+					vim.lsp.buf.signature_help()
+				end, opts)
+			end,
+		})
 	end,
 }
--- nvim/lua/main/lazy/trouble.lua
+++ nvim/lua/main/lazy/trouble.lua
@@ -3,21 +3,54 @@ return {
 		"folke/trouble.nvim",
 		config = function()
 			require("trouble").setup({
-				icons = false,
+				icons = true,
 			})
 
-			-- TODO - Set up trouble
-			vim.keymap.set("n", "<leader>tt", function()
-				require("trouble").toggle()
-			end)
+			vim.keymap.set("n", "<leader>cq", "<cmd>Trouble qflist toggle<cr>", { silent = true, noremap = true })
 
-			vim.keymap.set("n", "[t", function()
-				require("trouble").next({ skip_groups = true, jump = true })
-			end)
+			vim.keymap.set("n", "<leader>ce", "<cmd>Trouble diagnostics toggle<cr>", { silent = true, noremap = true })
 
-			vim.keymap.set("n", "]t", function()
-				require("trouble").previous({ skip_groups = true, jump = true })
-			end)
+			vim.keymap.set(
+				"n",
+				"<leader>cE",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				{ silent = true, noremap = true }
+			)
+
+			vim.keymap.set(
+				"n",
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=true win.position=bottom<cr>",
+				{ silent = true, noremap = true }
+			)
+
+			vim.keymap.set(
+				"n",
+				"gh",
+				"<cmd>Trouble lsp toggle focus=true auto_refresh=false<cr>",
+				{ silent = true, noremap = true }
+			)
+
+			vim.keymap.set(
+				"n",
+				"gH",
+				"<cmd>Trouble lsp_references toggle focus=true auto_refresh=false<cr>",
+				{ silent = true, noremap = true }
+			)
+
+			vim.keymap.set(
+				"n",
+				"gd",
+				"<cmd>Trouble lsp_definitions toggle focus=true auto_refresh=false<cr>",
+				{ silent = true, noremap = true }
+			)
+
+			vim.keymap.set(
+				"n",
+				"gI",
+				"<cmd>Trouble lsp_implementations toggle focus=true auto_refresh=false<cr>",
+				{ silent = true, noremap = true }
+			)
 		end,
 	},
 }
