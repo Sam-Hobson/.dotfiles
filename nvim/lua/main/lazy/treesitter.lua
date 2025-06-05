ea8fd85c3e697a86353fe65561cf340f7326895c
Sam <sam@halehobson.com>
2025-06-05 11:51:45 +1000

    Update nvim formatters

--- nvim/lua/main/lazy/treesitter.lua
+++ nvim/lua/main/lazy/treesitter.lua
@@ -71,7 +71,12 @@ return {
 				-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
 				separator = nil,
 				zindex = 20, -- The Z-index of the context window
-				on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+				on_attach = function(bufnr)
+					local ft = vim.bo[bufnr].filetype
+					if ft == "html" then
+						require("treesitter-context").disable()
+					end
+				end,
 			})
 		end,
 	},
